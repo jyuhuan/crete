a427eeaf071262b087dae9fb9a460e152d0e1d64
yuhuan <jyuhuan@gmail.com>
2015-07-01 22:44:18 -0400

    demonstrates that Sentence's hashCode and equals are working

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -2,14 +2,28 @@
  * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 7/1/15.
  */
 
+import crete.Sentence
 import crete.dsl._
 
-object Test extends App {
+object Test {
+
+
+  def main(args: Array[String]) {
+    val kb = Set[Sentence](
+      'smokes('p) implies 'cancer('p),
+      'cancer('x) implies 'die('p),
+      'cancer('x) implies 'die('p),
+      'cancer('x) implies 'die('p),
+      'cancer('x) implies 'die('p),
+      'smokes("john")
+    )
+    val query = kb entails 'die("john")
+
+    val sentence = 'smokes('p) && ~'drinks('p) || 'hasFun('p) && ~'jumps('p) implies 'cancer('p)
 
 
 
-  val sentence = 'smokes('p) ∧ ~'drinks('p) → 'cancer('p)
+    val bp = 0
 
-
-  val bp =0
+  }
 }
