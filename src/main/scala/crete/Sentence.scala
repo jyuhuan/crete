9f5751c9045fdc3b3d06bf0d777634cbd28eb724
yuhuan <jyuhuan@gmail.com>
2015-07-01 22:42:55 -0400

    changed sentence operators to ensure precedence. Deleted all truth value evaluation methods.

--- src/main/scala/crete/Sentence.scala
+++ src/main/scala/crete/Sentence.scala
@@ -4,10 +4,10 @@ package crete
  * Created by Yuhuan Jiang (jyuhuan@gmail.com) on 7/1/15.
  */
 sealed trait Sentence {
-  def ∧(that: Sentence) = BinaryCompoundSentence(this, "∧", that)
-  def ∨(that: Sentence) = BinaryCompoundSentence(this, "∨", that)
-  def →(that: Sentence) = BinaryCompoundSentence(this, "→", that)
-  def ↔(that: Sentence) = BinaryCompoundSentence(this, "↔", that)
+  def &&(that: Sentence) = BinaryCompoundSentence(this, "∧", that)
+  def ||(that: Sentence) = BinaryCompoundSentence(this, "∨", that)
+  def implies(that: Sentence) = BinaryCompoundSentence(this, "→", that)
+  def iff(that: Sentence) = BinaryCompoundSentence(this, "↔", that)
   def unary_~ = UnaryCompoundSentence("~", this)
 }
 
@@ -15,21 +15,19 @@ case class BinaryCompoundSentence(left: Sentence, op: String, right: Sentence) e
 case class UnaryCompoundSentence(op: String, operand: Sentence) extends Sentence
 case class QuantifiedSentence(quantifier: String, variables: Seq[Var], sentence: Sentence) extends Sentence
 
-trait Predicate extends Sentence {
-  def name: Symbol
-  def eval: Boolean
-}
 
-case class ParameterlessPredicate(name: Symbol) extends Predicate {
-  override def eval: Boolean = ???
+case class ParameterlessPredicate(name: Symbol) {
   def apply(t: Term) = UnaryPredicate(name, t)
   def apply(t1: Term, t2: Term) = BinaryPredicate(name, t1, t2)
 }
 
+
+trait Predicate extends Sentence {
+  def name: Symbol
+}
+
 case class UnaryPredicate(name: Symbol, t: Term) extends Predicate {
-  override def eval: Boolean = ???
 }
 
 case class BinaryPredicate(name: Symbol, t1: Term, t2: Term) extends Predicate {
-  override def eval: Boolean = ???
 }
